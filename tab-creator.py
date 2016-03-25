8b0702ca0dc1d75cbd74124ce4c59072258cb184
ikaikastine <ikaikastine@gmail.com>
2016-03-24 22:30:10 -0700

    added tab framework

--- tab-creator.py
+++ tab-creator.py
@@ -10,6 +10,7 @@ def setup():
 	song.replace(" ", "_")
 	song_name = song
 	song += '.txt'
+	print "songname: ", song
 	# Get the name of the artist
 	artist = raw_input("Please enter the artist: ")
 	# Set the tuning for the song
@@ -17,11 +18,28 @@ def setup():
 	# Sets path for files to be placed in tabs folder
 	path_name = 'tabs/'
 	path_to_file = os.path.join(path_name, song)
-
+	# Open the file for writing
 	f = open(path_to_file, 'w')
 	f.write("Author: %s\n" % name)
 	f.write("Song: %s\n" % song_name)
 	f.write("Artist: %s\n" % artist)
+	f.close()
+	# Call the framework function
+	framework(path_to_file, tuning)
+
+def framework(path_to_file, tuning):
+	tuning_strings = []
+	f = open(path_to_file, 'a')
+	if (tuning == 'standard'):
+		tuning_strings = ['E', 'A', 'D', 'G', 'B', 'E']
+	elif (tuning == 'drop-d'):
+		tuning_strings = ['D', 'A', 'D', 'G', 'B', 'E']
+
+	for i in range(0, len(tuning_strings)):
+		f.write("%s" % tuning_strings[i])
+		for i in xrange(1, 5):
+			f.write("|--------------------")
+		f.write("\n")
 
 if __name__ == "__main__":
 	setup()
