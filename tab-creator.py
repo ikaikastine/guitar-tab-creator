d06b6225ffca798f686525589cbca363adbba920
ikaikastine <ikaikastine@gmail.com>
2016-03-24 21:57:11 -0700

    add comments for clarity

--- tab-creator.py
+++ tab-creator.py
@@ -2,13 +2,19 @@
 import os.path
 
 def setup():
+	# Get user's name
 	name = raw_input("Please enter your name: ")
+	# Get song name
 	song = raw_input("Please enter the song name: ")
+	# If user gives whitespace, replace it with an underscore
 	song.replace(" ", "_")
 	song_name = song
 	song += '.txt'
+	# Get the name of the artist
 	artist = raw_input("Please enter the artist: ")
+	# Set the tuning for the song
 	tuning = raw_input("Please enter the song tuning (standard, drop-d, drop-c, d-standard): ")
+	
 	path_name = 'tabs/'
 	path_to_file = os.path.join(path_name, song)
 
